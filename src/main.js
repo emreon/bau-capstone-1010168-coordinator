943ed297d128ca3cb7b6bc58c17f9180d6de5f91
emreon <onrat.emre@gmail.com>
2022-04-25 19:53:14 +0300

    pi-camera test

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+    mode: 'photo',
+    width: 640,
+    height: 480,
+    nopreview: true,
+});
 
-console.log('start')
+myCamera
+    .snapDataUrl()
+    .then((result) => {
+        // Your picture was captured
+        console.log('<img src="${result}">');
+    })
+    .catch((error) => {
+        // Handle your error
+    });
